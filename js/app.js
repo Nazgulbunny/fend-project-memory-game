6eaf13afcac6a02886709980cf2e385ca0b9ae83
Max <maxsermi@gmail.com>
2018-02-08 18:34:43 +0100

    refactor Final refactor for all the code before submission

--- js/app.js
+++ js/app.js
@@ -1,43 +1,7 @@
-/*
- * Create a list that holds all of your cards
- */
-var deck =["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor",
-           "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf",
-           "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
 
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
+ * shuffle() function provided by Udacity's starter code
  */
-
- // function to loop through each card and create its HTML
-
- function makeCards(){
- 	deck = shuffle(deck);
- 	var index = 0;
- 	$.each($(".card i"),function(){
- 		$(this).attr("class","fa " + deck[index]);
- 		index++;
- 	});
- 	timeReset();
- };
-
-//make a random game board
-$(makeCards);
-
-//Function to reset everything to default state
-var resetGame = function() {
-    open = [];
-    matched = 0;
-    moveCounter = 0;
-    resetTimer();
-    updateMoveCounter();
-    $(".card").attr("class", "card");
-    makeCards();
-    resetStars();
-};
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -52,24 +16,109 @@ function shuffle(array) {
     }
 
     return array;
-}
+};
 
-// open selected card and show the state
-function openCard(card) {
-    if (!card.hasClass("open")) {
-        card.addClass("open");
-        card.addClass("show");
-        open.push(card);
+
+
+var deck = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor",
+           "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf",
+           "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
+
+// Game variables
+var open = [];
+var matched = 0;
+var moveCounter = 0;
+var numStars = 3;
+var timer = {
+    seconds: 0,
+    minutes: 0,
+    clearTime: -1
+};
+
+// Difficulty settings 
+var hard = 10;
+var medium = 15;
+
+var modal = $("#win-modal");
+
+
+
+// Timerfunction
+var startTimer = function() {
+    if (timer.seconds === 59) {
+        timer.minutes++;
+        timer.seconds = 0;
+    } else {
+        timer.seconds++;
+    }
+
+    
+    var formattedSec = "0";
+    if (timer.seconds < 10) {
+        formattedSec += timer.seconds
+    } else {
+        formattedSec = String(timer.seconds);
+    }
+
+    var time = String(timer.minutes) + ":" + formattedSec;
+    $(".timer").text(time);
+};
+
+// timer reset
+function resetTimer() {
+    clearInterval(timer.clearTime);
+    timer.seconds = 0;
+    timer.minutes = 0;
+    $(".timer").text("0:00");
+
+    timer.clearTime = setInterval(startTimer, 1000);
+};
+
+// Creates and update card HTML
+function updateCards() {
+    deck = shuffle(deck);
+    var index = 0;
+    $.each($(".card i"), function(){
+      $(this).attr("class", "fa " + deck[index]);
+      index++;
+    });
+    resetTimer();
+};
+
+//  win modal 
+function showModal() {
+    modal.css("display", "block");
+};
+
+// Removes stars
+function removeStar() {
+    $(".fa-star").last().attr("class", "fa fa-star-o");
+    numStars--;
+    $(".num-stars").text(String(numStars));
+};
+
+// Restores stars 
+function resetStars() {
+    $(".fa-star-o").attr("class", "fa fa-star");
+    numStars = 3;
+    $(".num-stars").text(String(numStars));
+};
+
+// Updates number of moves in the HTML
+function updateMoveCounter() {
+    $(".moves").text(moveCounter);
+
+    if (moveCounter === hard || moveCounter === medium) {
+        removeStar();
     }
 };
 
-//Function to check if a card is valid or not
+// Checks if card is a valid 
 function isValid(card) {
     return !(card.hasClass("open") || card.hasClass("match"));
 };
 
-//function to match a card
-
+// Returns a card match
 function checkMatch() {
     if (open[0].children().attr("class")===open[1].children().attr("class")) {
         return true;
@@ -78,15 +127,16 @@ function checkMatch() {
     }
 };
 
-//set back a card to default state
-var resetOpen = function() {
-    open.forEach(function(card) {
-        card.toggleClass("open");
-        card.toggleClass("show");
-    });
-    open = [];
+// Win condition
+function hasWon() {
+    if (matched === 16) {
+        return true;
+    } else {
+        return false;
+    }
 };
-//function that checks if 2 cards are a match
+
+//  Checks win condition
 var setMatch = function() {
     open.forEach(function(card) {
         card.addClass("match");
@@ -100,9 +150,39 @@ var setMatch = function() {
     }
 };
 
-//event listener
-$(".card").click(onClick);
-//function binded to the event listener for game mecchanics
+// Reset cards
+var resetOpen = function() {
+    open.forEach(function(card) {
+        card.toggleClass("open");
+        card.toggleClass("show");
+    });
+    open = [];
+};
+
+// Sets selected card to the open and shown state
+function openCard(card) {
+    if (!card.hasClass("open")) {
+        card.addClass("open");
+        card.addClass("show");
+        open.push(card);
+    }
+};
+
+
+
+// Resets game
+var resetGame = function() {
+    open = [];
+    matched = 0;
+    moveCounter = 0;
+    resetTimer();
+    updateMoveCounter();
+    $(".card").attr("class", "card");
+    updateCards();
+    resetStars();
+};
+
+// Primary game logic function
 var onClick = function() {
     if (isValid( $(this) )) {
 
@@ -115,110 +195,27 @@ var onClick = function() {
             updateMoveCounter();
 
             if (checkMatch()) {
-                setTimeout(setMatch, 200);
+                setTimeout(setMatch, 300);
 
             } else {
-                setTimeout(resetOpen, 600);
+                setTimeout(resetOpen, 700);
 
             }
         }
     }
 };
 
-//win condition
-function hasWon() {
-    if (matched === 16) {
-        return true;
-    } else {
-        return false;
-    }
-};
-
-//event listener to reset game
-$(".restart").click(resetGame);
-
-//function to reset the game
+// Play again function
 var playAgain = function() {
     resetGame();
     modal.css("display", "none");
 };
 
-//Function to increment the timer
-var startTimer = function() {
-    if (timer.seconds === 59) {
-        timer.minutes++;
-        timer.seconds = 0;
-    } else {
-        timer.seconds++;
-    }
+//event listeners
 
-//function to reset the timer
-function resetTimer() {
-    clearInterval(timer.clearTime);
-    timer.seconds = 0;
-    timer.minutes = 0;
-    $(".timer").text("0:00");
-
-    timer.clearTime = setInterval(startTimer, 1000);
-};
-
-//Function to update moves and to remove stars
-function updateMoveCounter() {
-    $(".moves").text(moveCounter);
-
-    if (moveCounter === hard || moveCounter === medium) {
-        removeStar();
-    }
-};
-
-//LAst star remover
-function removeStar() {
-    $(".fa-star").last().attr("class", "fa fa-star-o");
-    numStars--;
-    $(".num-stars").text(String(numStars));
-};
-
-//Restore stars
-function resetStars() {
-    $(".fa-star-o").attr("class", "fa fa-star");
-    numStars = 3;
-    $(".num-stars").text(String(numStars));
-};
-
-//win modal function
-function showModal() {
-    modal.css("display", "block");
-};
-
-//Play again event listener
+$(".card").click(onClick);
+$(".restart").click(resetGame);
 $(".play-again").click(playAgain);
 
-//Variables
-var open = [];
-var matched = 0;
-var moveCounter = 0;
-
-
-//Variable for difficulty and timer
-var numStars = 3;
-var timer = {
-    seconds: 0,
-    minutes: 0,
-    clearTime: -1
-};
-
-//Variable for difficulty + win modal
-
-var hard = 15;
-var medium = 20;
-var modal = $("#win-modal");
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// Provides a randomized game board on page load
+$(updateCards);
